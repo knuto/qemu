1e739fcd96123cbfba694cbb5dbdc42d163faf95
Knut Omang <knut.omang@oracle.com>
2014-08-21 19:08:07 +0200

    pcie: Add support for Single Root I/O Virtualization (SR/IOV)
    This patch provides the building blocks for creating an SR/IOV
    PCIe Extended Capability header and register/unregister
    SR/IOV Virtual Functions.

--- include/hw/pci/pcie.h
+++ include/hw/pci/pcie.h
@@ -25,6 +25,7 @@
 #include "hw/pci/pci_regs.h"
 #include "hw/pci/pcie_regs.h"
 #include "hw/pci/pcie_aer.h"
+#include "hw/pci/pcie_sriov.h"
 #include "hw/hotplug.h"
 
 typedef enum {
@@ -82,6 +83,11 @@ struct PCIExpressDevice {
 
     /* ACS */
     uint16_t acs_cap;
+
+    /* SR/IOV */
+    uint16_t sriov_cap;
+    PCIESriovPF sriov_pf;
+    PCIESriovVF sriov_vf;
 };
 
 #define COMPAT_PROP_PCP "power_controller_present"
